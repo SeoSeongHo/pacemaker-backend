8fb1187d0cc7329d8b9e391d64c69eb4253c4325
DOWN-LEE <daun7137@gmail.com>
2021-11-27 19:08:21 +0900

    in game

---
+++ pacemaker-backend/src/test/kotlin/com/snucse/pacemaker/service/match/MatchServiceImplTest.kt
@@ -0,0 +1,12 @@
+package com.snucse.pacemaker.service.match
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MatchServiceImplTest {
+
+    @Test
+    fun findMatchByUserId() {
+    }
+}
