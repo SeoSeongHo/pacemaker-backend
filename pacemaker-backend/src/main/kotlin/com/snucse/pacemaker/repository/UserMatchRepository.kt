953e5e5c623cec1b9028fc6f56d9f14da25725ee
SeoSeongHo <swa04246@naver.com>
2021-11-27 19:07:47 +0900

    temp commit

---
+++ pacemaker-backend/src/main/kotlin/com/snucse/pacemaker/repository/UserMatchRepository.kt
@@ -0,0 +1,10 @@
+package com.snucse.pacemaker.repository
+
+import com.snucse.pacemaker.domain.UserMatch
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserMatchRepository: JpaRepository<UserMatch, Long> {
+
+    fun existsByUser_Id(userId: Long): Boolean
+    fun findByUser_Id(userId: Long): UserMatch?
+}
