2b01551c3aafe5cb02655f7f42a09ed66e2e1ce1
DOWN-LEE <daun7137@gmail.com>
2021-11-29 01:48:16 +0900

    update inMatchPolling and userHistory

--- pacemaker-backend/src/main/kotlin/com/snucse/pacemaker/repository/UserHistoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.snucse.pacemaker.repository
-
-import com.snucse.pacemaker.domain.UserHistory
-import com.snucse.pacemaker.domain.UserMatch
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserHistoryRepository: JpaRepository<UserHistory, Long> {
-    fun findAllByUser_Id(userId: Long): List<UserHistory>
-}
--- pacemaker-backend/src/main/kotlin/com/snucse/pacemaker/service/match/MatchService.kt
+++ pacemaker-backend/src/main/kotlin/com/snucse/pacemaker/service/match/MatchService.kt
@@ -1,12 +1,15 @@
 package com.snucse.pacemaker.service.match
 
 import com.snucse.pacemaker.domain.Match
+import com.snucse.pacemaker.domain.UserMatch
 
 import com.snucse.pacemaker.dto.MatchDto
 
 interface MatchService {
 
     fun match(matchReq: MatchDto.MatchReq, userId: Long): MatchDto.MatchRes
-  fun inMatchPolling(userId: Long, inMatchReq: InMatchReq): InMatchRes
+
+    fun getUserMatchByUserMatchId(userMatchId: Long): UserMatch
+    fun inMatchPolling(inMatchReq: MatchDto.InMatchReq): MatchDto.InMatchRes
 
 }
